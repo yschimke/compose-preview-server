00b977ff3f51430b6d579d7fc3d648965db4b783
Yuri Schimke <yuri@schimke.ee>
2026-09-12 14:50:22 +0100

    chore(deps): combine the four open experiment dependency bumps (#790)

--- experiments/remote-state-selection/build.gradle.kts
+++ experiments/remote-state-selection/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("com.android.library") version "9.3.1"
+  id("com.android.library") version "9.4.0"
   id("org.jetbrains.kotlin.plugin.compose") version "2.4.20"
 }
 
@@ -16,12 +16,12 @@ kotlin {
 }
 
 dependencies {
-  implementation("androidx.compose.remote:remote-creation-compose:1.0.0-alpha18")
-  testImplementation("androidx.compose.remote:remote-player-view:1.0.0-alpha18")
-  testImplementation("androidx.compose.remote:remote-core:1.0.0-alpha18")
-  testImplementation("androidx.compose.remote:remote-player-core:1.0.0-alpha18")
+  implementation("androidx.compose.remote:remote-creation-compose:1.0.0-alpha19")
+  testImplementation("androidx.compose.remote:remote-player-view:1.0.0-alpha19")
+  testImplementation("androidx.compose.remote:remote-core:1.0.0-alpha19")
+  testImplementation("androidx.compose.remote:remote-player-core:1.0.0-alpha19")
   testImplementation("androidx.test:core:1.7.0")
-  testImplementation("org.robolectric:robolectric:4.16.1")
+  testImplementation("org.robolectric:robolectric:4.17")
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.11.0")
   testImplementation("junit:junit:4.13.2")
 }
